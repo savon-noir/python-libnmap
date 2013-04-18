9e35a956ca2a20fd5a95f3da80a32834f165f604
Ronald <mini.pelle@gmail.com>
2013-04-18 16:37:57 +0200

    added get/delete method for mongol db plugin

--- libnmap/plugins/mongodb.py
+++ libnmap/plugins/mongodb.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from libnmap.plugins.dbplugin import NmapDBPlugin
 from pymongo import MongoClient
+from bson.objectid import ObjectId
 
 class NmapMongoPlugin(NmapDBPlugin):
     def __init__(self, **kwargs):
@@ -10,3 +11,20 @@ class NmapMongoPlugin(NmapDBPlugin):
 
     def db_insert(self, dict_data):
         self.collection.insert(dict_data)
+
+    def db_get(self, report_id=None):
+        rid = report_id
+        if report_id is not None and isinstance(report_id, str):
+            rid = ObjectId(report_id)
+
+        if isinstance(rid, ObjectId):
+            r = self.collection.find({ '_id': rid })
+        else:
+            r = self.collection.find()
+        return r
+
+    def db_delete(self, report_id=None):
+        if report_id is not None and isinstance(report_id, str):
+            ir = self.collection.remove({ '_id': ObjectId(report_id)})
+        else:
+            r= self.collection.remove({ '_id': report_id })
