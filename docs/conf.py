92e726df7ce3d7ffd1e81095826db05cb50e65ec
Ronald <mini.pelle@gmail.com>
2013-05-18 04:33:53 +0200

    changed copyright in documentation

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'libnmap'
-copyright = u'2013, Ronald Bister'
+copyright = u'CC-BY 2013, Ronald Bister'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
