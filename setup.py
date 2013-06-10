e4b23714fe454639bdd0fda2d9a6d20be57128e0
Ronald <mini.pelle@gmail.com>
2013-06-11 01:05:26 +0200

    prepare for pypi

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst") as rfile:
 
 setup(
     name='python-libnmap',
-    version='0.2.5',
+    version='0.2.7',
     author='Ronald Bister',
     author_email='mini.pelle@gmail.com',
     packages=['libnmap', 'libnmap.plugins', 'libnmap.test'],
