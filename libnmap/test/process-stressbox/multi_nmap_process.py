2522c5ce2969a3a4cdd8e9480d1f837a03458bfd
Ronald <mini.pelle@gmail.com>
2014-04-06 19:33:23 +0200

    fixed pyflakes stuff

--- libnmap/test/process-stressbox/multi_nmap_process.py
+++ libnmap/test/process-stressbox/multi_nmap_process.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
  
 from libnmap.process import NmapProcess
-from time import sleep
  
 def make_nmproc_obj(targets, options):
     return NmapProcess(targets=targets, options=options)
