c1f489f2af95366408bce97267a2777e75e546ec
Ronald <mini.pelle@gmail.com>
2014-06-18 03:38:09 +0200

    added test case for process.py

--- libnmap/__init__.py
+++ libnmap/__init__.py
@@ -5,4 +5,4 @@ __credits__ = ['Ronald Bister', 'Mike Boutillier']
 __maintainer__ = 'Ronald Bister'
 __email__ = 'mini.pelle@gmail.com'
 __license__ = 'CC-BY'
-__version__ = '0.5.1'
+__version__ = '0.6'
